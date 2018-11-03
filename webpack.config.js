b85e679f71726ac6293cc439194b33d925f2ec84
Shane EC Davis <shaneecdavis7@gmail.com>
2018-11-03 15:24:11 -0500

    almost finished added a lot of babel to reach it to stage-2

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,38 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+const HtemlWebPackPlugin = require('html-webpack-plugin')
+
+
 let path = require('path')
 let nodeExternals = require('webpack-node-externals')
 const moduleObj = {
-  loaders: [
+  rules: [
     {
       test: /\.js$/,
       exclude: /node_modules/,
-      loaders: ['babel-loader']
+      loaders: 'babel-loader'
     }
   ]
 }
 const client = {
+  mode: isDev ? 'development' : 'production',
   entry: {
     client: './src/client/index.js'
   },
   target: 'web',
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist/public')
   },
-  module: moduleObj
+  module: moduleObj,
+  plugins: [
+    new HtemlWebPackPlugin({
+      template: src/client/index.html
+    })
+  ]
 }
 const server = {
+  mode: isDev ? 'development' : 'production',
   entry: {
     server: './src/server/index.js'
   },
